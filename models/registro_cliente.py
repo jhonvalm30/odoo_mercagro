ef6c98071c6edc0bca2914cb1ab498175d0270cd
jhonvalm <jhonatan0430@hotmail.com>
2019-05-04 16:09:54 -0500

    Fix errors - 04/05/2019

--- models/registro_cliente.py
+++ models/registro_cliente.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class registro_cliente(models.Model):
     _name = 'registro.cliente'
     _rec_name = 'name'
-    cliente.id = fields.Integer('ID', required=True)
+    cliente_id = fields.Integer('ID', required=True)
     cedula = fields.Char('Cédula', required=True)
     name = fields.Char('Nombre', size=100, required=True)
     telefono = fields.Char('Telèfono celular', required=True)
