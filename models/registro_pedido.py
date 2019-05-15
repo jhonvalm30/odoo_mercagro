0d308f2fcfba44eebac4ef134726d9f6ef0b4334
jhonvalm <jhonatan0430@hotmail.com>
2019-05-14 21:27:31 -0500

    Fix model resgistro_pedido

--- models/registro_pedido.py
+++ models/registro_pedido.py
@@ -5,7 +5,7 @@ from . import registro_cliente, registro_finca, registro_producto
 class registro_pedido(models.Model):
     _name = 'registro.pedido'
     _rec_name = 'name'
-    name = fields.Many2one('registro.finca', 'Finca o lugar donde sale el prouducto', select=True, track_visibility='onchange')
+    name = fields.Many2one('registro.finca', 'Finca donde sale el prouducto', select=True, track_visibility='onchange')
     cliente_id = fields.Many2one('registro.cliente', 'Cliente', select=True, track_visibility='onchange')
     prouducto_id = fields.Many2one('registro.producto', 'Producto', select=True, track_visibility='onchange')
     autorizado = fields.Boolean('¿Salida Autorizada?', default=False, required=True)
